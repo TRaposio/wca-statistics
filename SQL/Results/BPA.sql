4cb28be6d74e5399080dda0974df2ce5d8980455
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2022-06-30 13:14:46 +0200

    Rounded avgs

--- SQL/Results/BPA.sql
+++ SQL/Results/BPA.sql
@@ -14,15 +14,15 @@ SELECT
    IF(
       eventId IN ('222','333','444','555','pyram','skewb','minx','333oh','clock', 'sq1'), 
       CASE
-         WHEN value1<0 THEN (value2+value3+value4)/300
-         WHEN value2<0 THEN (value1+value3+value4)/300 
-         WHEN value3<0 THEN (value2+value1+value4)/300 
-         WHEN value4<0 THEN (value2+value3+value1)/300 
-         ELSE (value1+value2+value3+value4-GREATEST(value1,value2,value3,value4))/300
+         WHEN value1<0 THEN ROUND((value2+value3+value4)/300,2)
+         WHEN value2<0 THEN ROUND((value1+value3+value4)/300,2)
+         WHEN value3<0 THEN ROUND((value2+value1+value4)/300,2)
+         WHEN value4<0 THEN ROUND((value2+value3+value1)/300,2)
+         ELSE ROUND((value1+value2+value3+value4-GREATEST(value1,value2,value3,value4))/300,2)
       END, 
       (value1+value2) / 100
       ) as BPA,
-   average/100 as average
+   ROUND(average/100,2) as average
 FROM 
    Results
 WHERE 
