9910d537bb8272521852bff633737d46011223ef
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2022-08-26 15:11:36 +0200

    Add files via upload

---
+++ SQL/Competitions/days_since_last_gold(recent).sql
@@ -0,0 +1,33 @@
+SELECT 
+	*, 
+	DATEDIFF(CURDATE(),t.last_gold) as dslg
+FROM (
+	SELECT 
+		r.personId, 
+		r.personName,
+		MAX(c.end_date) as last_gold
+	FROM 
+		Results r, 
+		Competitions c
+	WHERE 
+		c.id = r.competitionId AND 
+		r.countryId = 'Italy' AND
+		r.roundTypeId IN ('c','f') AND
+		r.pos = 1 AND
+		r.best > 0 AND
+		r.personId IN (
+			SELECT 
+				DISTINCT(r2.personId)
+			FROM 
+				Results r2, 
+				Competitions c2
+			WHERE 
+				r2.competitionId = c2.id AND
+				r2.countryId = 'Italy' AND
+				c.year > 2017
+			)
+	GROUP BY 
+		r.personId
+	) t
+ORDER BY 
+	dslg DESC
