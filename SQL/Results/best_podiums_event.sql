f6e072c27dcc52e6e7c4f3d65b6b238c09dad79d
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2022-10-18 15:11:18 +0200

    Update best_podiums_event.sql

--- SQL/Results/best_podiums_event.sql
+++ SQL/Results/best_podiums_event.sql
@@ -1,12 +1,12 @@
 SELECT 
 	c.id as competition, 
-	wca_statistics_time_format(SUM(r.average), 'r.eventId', 'average') as podium,
+	IF(r.eventId = '333fm', ROUND(SUM(r.average)/100,2), wca_statistics_time_format(SUM(r.average), 'r.eventId', 'average')) as podium,
 	GROUP_CONCAT(IF(r.pos=1,r.personName, NULL)) as first,
-    GROUP_CONCAT(IF(r.pos=1,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result1,
+        GROUP_CONCAT(IF(r.pos=1,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result1,
 	GROUP_CONCAT(IF(r.pos=2,r.personName, NULL)) as second,
-    GROUP_CONCAT(IF(r.pos=2,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result2,
+        GROUP_CONCAT(IF(r.pos=2,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result2,
 	GROUP_CONCAT(IF(r.pos=3,r.personName, NULL)) as third,
-    GROUP_CONCAT(IF(r.pos=3,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result3
+        GROUP_CONCAT(IF(r.pos=3,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result3
 FROM 
 	Competitions c, 
 	Results r
@@ -20,6 +20,6 @@ WHERE
 GROUP BY 
 	c.id
 HAVING
-    COUNT(DISTINCT personId) = 3
+        COUNT(DISTINCT personId) = 3
 ORDER BY 
-	2 ASC
+	SUM(r.average) ASC
