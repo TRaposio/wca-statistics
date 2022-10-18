25144fcf3d8f1c6e4f4e3ec5b80f584c2a72a065
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2022-10-18 12:02:42 +0200

    Add files via upload

---
+++ SQL/Results/best_podiums_event.sql
@@ -0,0 +1,25 @@
+SELECT 
+	c.id as competition, 
+	wca_statistics_time_format(SUM(r.average), 'r.eventId', 'average') as podium,
+	GROUP_CONCAT(IF(r.pos=1,r.personName, NULL)) as first,
+    GROUP_CONCAT(IF(r.pos=1,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result1,
+	GROUP_CONCAT(IF(r.pos=2,r.personName, NULL)) as second,
+    GROUP_CONCAT(IF(r.pos=2,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result2,
+	GROUP_CONCAT(IF(r.pos=3,r.personName, NULL)) as third,
+    GROUP_CONCAT(IF(r.pos=3,wca_statistics_time_format(r.average, 'r.eventId', 'average'), NULL)) as result3
+FROM 
+	Competitions c, 
+	Results r
+WHERE 
+	c.id = r.competitionId AND 
+	r.eventId = ':event' AND 
+	r.pos < 4 AND 
+	r.average > 0 AND 
+	r.roundTypeId IN ('c','f') AND 
+	c.countryId = ':country'
+GROUP BY 
+	c.id
+HAVING
+    COUNT(DISTINCT personId) = 3
+ORDER BY 
+	2 ASC
