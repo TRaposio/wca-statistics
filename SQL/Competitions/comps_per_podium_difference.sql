c987228ac1e9bfd3d6845f090bcf14f1e55c58c1
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2022-10-26 23:18:51 +0200

    Add files via upload

---
+++ SQL/Competitions/comps_per_podium_difference.sql
@@ -0,0 +1,18 @@
+WITH podium AS(
+SELECT r.competitionId, r.eventId, SUM(r.average) as podi
+FROM Results r, Competitions c
+WHERE r.competitionId = c.id
+AND r.eventId IN ('pyram','222')
+AND r.pos < 4
+AND r.roundTypeId IN ('c','f')
+AND r.average > 0
+GROUP BY r.competitionId, r.eventId
+HAVING COUNT(r.pos) = 3)
+
+SELECT pyra.competitionId, ROUND(pyra.podi/100,2) as pyrapodium, ROUND(due.podi/100,2) as duepodium, ROUND((due.podi - pyra.podi)/100,2) as delta
+FROM podium as pyra, podium as due
+WHERE pyra.eventId = 'pyram' AND due.eventId = '222' AND pyra.competitionId = due.competitionId
+ORDER BY delta DESC
+
+
+
