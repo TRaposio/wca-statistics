1ef59bd612685712f136b6b6a7915d3545e7e2ac
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2023-11-08 19:55:36 +0100

    Update female_competitors_comp_number_distribution.sql

--- SQL/Competitions/female_competitors_comp_number_distribution.sql
+++ SQL/Competitions/female_competitors_comp_number_distribution.sql
@@ -1,21 +1,22 @@
+/* distribution of the number of competitions of female competitors */ 
+
 WITH comps AS(
 	SELECT 
-		r.personId as id, 
+		r.personId as wca_id, 
 		COUNT(DISTINCT r.competitionId) as compN
 	FROM 
 		Results r
 	GROUP BY 
 		r.personId
 )
-
 SELECT 
 	c.compN,
-	COUNT(DISTINCT c.id) as competitors
+	COUNT(DISTINCT c.wca_id) as f_competitors
 FROM 
 	comps c, 
 	Persons p
 WHERE 
-	c.id = p.id AND 
+	c.wca_id = p.wca_id AND 
 	p.gender = 'f'
 GROUP BY 
 	c.compN
