1fd0fb2c40437982270a21c74d255157122f2351
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2023-11-08 21:02:45 +0100

    Update NR_distribution.sql

--- SQL/Results/NR_distribution.sql
+++ SQL/Results/NR_distribution.sql
@@ -1,4 +1,4 @@
-#Super easy code
+/* Distribution of national records */
 
 SELECT 
 	t.personId, 
@@ -28,7 +28,7 @@ FROM
 	Persons p
 
 WHERE 
-	p.id = t.personId AND 
+	p.wca_id = t.personId AND 
 	p.subId = 1 AND 
 	p.countryId = ':country'
 GROUP BY 
@@ -36,72 +36,4 @@ GROUP BY
 ORDER BY 
 	NRs DESC
 
-
-# Super long and dumb code, outer joins were necessary to simulate a full
-# outer join since people with either only single records or average
-# records get lost in a simple join
-
-
-WITH
-avg AS(
-	SELECT
-		personId, 
-		COUNT(DISTINCT eventId) as na
-	FROM 
-		RanksAverage
-	WHERE 
-		countryRank = 1 AND
-		eventId NOT IN ('magic','mmagic','333ft','333mbo')
-	GROUP BY 
-		personId
-),
-single AS(
-	SELECT
-		personId, 
-		COUNT(DISTINCT eventId) as ns
-	FROM 
-		RanksSingle
-	WHERE 
-		countryRank = 1 AND
-		eventId NOT IN ('magic','mmagic','333ft','333mbo')
-	GROUP BY 
-		personId
-)
-SELECT 
-	p.id, 
-	p.name, 
-	p.countryId, 
-	t.ns + t.na as total_NR
-FROM
-	(SELECT 
-		s.personId, 
-		IF(s.ns IS NULL, 0, s.ns) as ns, 
-		IF(a.na IS NULL, 0, a.na) as na
-	FROM 
-		single s
-	LEFT OUTER JOIN 
-		avg a 
-	ON 
-		s.personId = a.personId
-
-	UNION
-
-	SELECT 
-		a.personId, 
-		IF(s.ns IS NULL, 0, s.ns) as ns, 
-		IF(a.na IS NULL, 0, a.na) as na
-	FROM 
-		avg a
-	LEFT OUTER JOIN 
-		single s 
-	ON 
-		s.personId = a.personId) t, 
-	Persons p
-
-WHERE 
-	p.subId = 1 AND 
-	p.countryId = 'Italy' AND 
-	p.id = t.personId
-ORDER BY 
-	total_NR DESC
 
