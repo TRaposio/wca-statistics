0a14e76bd8d78b782b49f7d86cd4b850f74cfe1b
Tommaso Raposio <108098686+TRaposio@users.noreply.github.com>
2023-11-09 00:23:20 +0100

    Update fastest_name(at_least_5_persons).sql

--- SQL/Names/fastest_name(at_least_5_persons).sql
+++ SQL/Names/fastest_name(at_least_5_persons).sql
@@ -1,3 +1,5 @@
+/* Average of all people that share the same name (at least 5) */
+
 SELECT 
 	t.name, 
 	ROUND(AVG(t.best)/100,2) as avg, 
@@ -12,7 +14,7 @@ FROM (
 	JOIN 
 		Persons p
 	ON 
-		r.personid = p.id
+		r.personid = p.wca_id
 	WHERE 
 		r.eventId = '333' AND 
 		p.countryId = 'Italy'
